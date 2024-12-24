3ebb566feaf7c563d0374a86ac33de45986584ea
javierbull <javierbullreyes@gmail.com>
2024-12-24 12:00:30 -0500

    add function show hide password

--- js/login.js
+++ js/login.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
 
+    $(".toggle-password").click(function() {
+        $(this).toggleClass("fa-eye fa-eye-slash");
+        input = $(this).parent().find("input");
+        if (input.attr("type") == "password") {
+            input.attr("type", "text");
+        } else {
+            input.attr("type", "password");
+        }
+    });
 });
