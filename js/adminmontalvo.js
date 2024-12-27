fd64d1e53bf6b2979beed8af057761b063efa930
javierbull <javierbullreyes@gmail.com>
2024-12-27 16:08:44 -0500

    activo select2

--- js/adminmontalvo.js
+++ js/adminmontalvo.js
@@ -83,4 +83,13 @@ $(document).ready(function () {
     });
     // End show panels admin
 
+    $('.select2').each(function () {
+        $(this).select2({
+            theme: 'bootstrap-5',
+            width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
+            placeholder: $(this).data('placeholder'),
+            dropdownParent: $(this).parent()
+        });
+    });
+
 });
